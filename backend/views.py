bfefda3721775d78a4031a63eb4f2e1043c0bea3
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-24 22:23:41 +0300

    added views

--- backend/views.py
+++ backend/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, )
+    return render(request, "index.html")
+
+
+def services(request):
+    return render(request, "services.html")
